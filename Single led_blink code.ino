fdc676580bde6580c0a30f385fa78e71644c7df4
passion-tech <62792837+passion-tech@users.noreply.github.com>
2020-03-28 19:36:05 +0800

    Initial commit

---
+++ Single led_blink code.ino
@@ -0,0 +1,15 @@
+
+void setup() {
+  pinMode(12,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(12,HIGH);
+  delay(500);
+  digitalWrite(12,LOW);
+  delay(1500);
+  // put your main code here, to run repeatedly:
+
+}
