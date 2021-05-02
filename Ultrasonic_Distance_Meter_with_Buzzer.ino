e2827dd3042a3d2c69f976514e2cca86bef2dd9a
passion-tech <62792837+passion-tech@users.noreply.github.com>
2021-05-02 21:41:59 +0800

    Innitial commit

---
+++ Ultrasonic_Distance_Meter_with_Buzzer.ino
@@ -0,0 +1,50 @@
+const int trigPin = 11;
+const int echoPin = 12;
+const int buzzer = 2;
+const int ledPin = 3;
+
+// defines variables
+long duration;
+int distance;
+int safetyDistance;
+
+
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+pinMode(buzzer, OUTPUT);
+pinMode(ledPin, OUTPUT);
+Serial.begin(9600); // Starts the serial communication
+}
+
+
+void loop() {
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+distance= duration*0.034/2;
+
+safetyDistance = distance;// here is you can adjust the range of distance as you like.
+if (safetyDistance <= 10){
+  digitalWrite(buzzer, HIGH);
+  digitalWrite(ledPin, HIGH);
+}
+else{
+  digitalWrite(buzzer, LOW);
+  digitalWrite(ledPin, LOW);
+}
+
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+}
