5ac2ded3a9e23dbd5f97f45c1e5352dbe52c370c
passion-tech <62792837+passion-tech@users.noreply.github.com>
2021-06-18 18:03:16 +0800

    initial commit

---
+++ Control_Servo_with_Potential_Meter.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int potpin = A0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+
+void setup() {
+  Serial.begin(9600);          //  setup serial
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  Serial.print(val);
+  Serial.print(" ");
+  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  myservo.write(val);                  // sets the servo position according to the scaled value
+  Serial.print(val);
+  Serial.println();
+  delay(50);                           // waits for the servo to get there
+}
+ 
