19c9bd1ceb5f135d758e05877aa3ff8f4ba5f7f6
passion-tech <62792837+passion-tech@users.noreply.github.com>
2024-04-14 19:55:47 +0800

    Initial commit

---
+++ RFID_LOCK_With_LCD.ino
@@ -0,0 +1,90 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16,2);
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+#define LED_G 5 //define green LED pin
+#define LED_R 4 //define red LED
+#define RELAY 3 //relay pin
+#define BUZZER 2 //buzzer pin
+#define ACCESS_DELAY 2000
+#define DENIED_DELAY 1000
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(9600);
+  lcd.backlight();   // Initiate a serial communication
+  SPI.begin();          // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(RELAY, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  noTone(BUZZER);
+  digitalWrite(RELAY, HIGH);
+  lcd.begin(16,2);
+  lcd.setCursor(0,0); 
+  lcd.print("Scan your card");
+  Serial.println("Scan your card...");
+  Serial.println();
+  
+  
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "2B FE 4C 0C") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    lcd.setCursor(0,1); 
+  lcd.print("Access Granted");
+    delay(500);
+    digitalWrite(RELAY, LOW);
+    digitalWrite(LED_G, HIGH);
+    delay(ACCESS_DELAY);
+    digitalWrite(RELAY, HIGH);
+    digitalWrite(LED_G, LOW);
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    lcd.setCursor(0,1); 
+  lcd.print("Access denied.");
+    digitalWrite(LED_R, HIGH);
+    tone(BUZZER, 300);
+    delay(DENIED_DELAY);
+    digitalWrite(LED_R, LOW);
+    noTone(BUZZER);
+  }
+  
+    lcd.setCursor(0,1);
+    lcd.print("................");
+}
